e659d90adb2f0bb1b5953b915185ea0ef38a7314
Duong Dang <dddangmedia@gmail.com>
2020-02-21 18:51:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
       
       
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 == 0 && int % 5 == 0 
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
-  else int % 3 == 0 && int % 5 == 0 
+  else int % 3 == 0
     "FizzBuzz"
   end
   
